e1df840d8bf1091f74c9cdb66a3f2ff906b58bcd
Michael Delfino <m_delfino@outlook.com>
2024-10-26 20:58:31 -0400

    now fills a backbuffer for rendering

--- src/win32_scratch.cpp
+++ src/win32_scratch.cpp
@@ -1,45 +1,106 @@
 #include "windows.h"
 
 static bool Running;
+static BITMAPINFO BitmapInfo;
+static void *BitmapMemory;
+static HBITMAP BitmapHandle;
+static HDC BitmapDeviceContext;
 
-LRESULT MainWindowCallback(HWND Window, UINT Message, WPARAM wParam, LPARAM lParam)
+static void ResizeDIBSection(int width, int height)
+{
+    if (BitmapHandle) {
+        DeleteObject(BitmapHandle);
+    }
+    if (!BitmapDeviceContext) {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = width;
+    BitmapInfo.bmiHeader.biHeight = height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext, 
+        &BitmapInfo,
+        DIB_RGB_COLORS, 
+        &BitmapMemory,
+        0, 0);
+}
+
+static void UpdateWindow(HDC DeviceContext, int x, int y, int width, int height)
+{
+    StretchDIBits(
+            DeviceContext, 
+            x, y, width, height,
+            x, y, width, height,
+            BitmapMemory,
+            &BitmapInfo,
+            DIB_RGB_COLORS,
+            SRCCOPY
+    );
+}
+
+LRESULT MainWindowCallback(HWND Window, 
+                           UINT Message, 
+                           WPARAM wParam, 
+                           LPARAM lParam)
 {
     LRESULT Result = 0;
     switch(Message) {
         case WM_SIZE:
-            OutputDebugStringA("WM_Size\n");
-            break;
+        {
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int w = ClientRect.right - ClientRect.left;
+            int h = ClientRect.bottom - ClientRect.top;
+            ResizeDIBSection(w, h);
+        }break;
+
         case WM_DESTROY:
-	    Running = false;
-            break;
+        {
+            Running = false;
+        }break;
+
         case WM_CLOSE:
-	    Running = false;
-            break;
+        {
+            Running = false;
+        }break;
+
         case WM_ACTIVATEAPP:
+        {
             OutputDebugStringA("WM_ActivateApp\n");
-            break;
+        }break;
+
         case WM_PAINT:
         {
             PAINTSTRUCT Paint;
             HDC DeviceContext = BeginPaint(Window, &Paint);
             
-            int X = Paint.rcPaint.left;
-            int Y = Paint.rcPaint.top;
-            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS); 
+            int x = Paint.rcPaint.left;
+            int y = Paint.rcPaint.top;
+            int width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            UpdateWindow(DeviceContext, x, y, width, height);
 
             EndPaint(Window, &Paint);
         } break;
+
         default:
+        {
             // OutputDebugStringA("default\n");
             Result = DefWindowProc(Window, Message, wParam, lParam);
-            break;
+        }break;
     }
     return Result;
 }
 
-int WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode)
+int WinMain(HINSTANCE Instance, 
+            HINSTANCE PrevInstance, 
+            LPSTR CommandLine, 
+            int ShowCode)
 {
     WNDCLASSA WindowClass = {};
 
